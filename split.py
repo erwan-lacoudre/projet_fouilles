48e2934b4831e671d3c99c3b2f9fc0e01b95ebda
Nilam De Oliveira-Gill <nilam@deol.eu>
2022-12-10 11:17:23 +0100

    Classification 1.1

--- split.py
+++ split.py
@@ -5,6 +5,9 @@ def split_lines(input, seed, output1, output2):
   with open(file = input, mode='r') as file:
     with open(file = output1, mode='w+') as out1:
       with open(file = output2, mode='w+') as out2:
+        names = file.readline()
+        out1.write(names)
+        out2.write(names)
         for line in file.readlines():
           if random.random() <= 0.75:
             out1.write(line)
